901ec149d68dc89ac8d9107d3806beec0ff273a5
Meta Engineer <m@plankc.com>
2021-03-18 22:55:32 -0600

    Update functions return types

--- prism_api/state_manager/store.py
+++ prism_api/state_manager/store.py
@@ -15,13 +15,13 @@ def deserialize_state(state_serialized: str) -> dict:
     return json.loads(state_serialized)
 
 
-async def save_state(client_id, state: dict):
+async def save_state(client_id, state: dict) -> dict:
     store = Store(client_id)
     state_serialized = serialize_state(state)
     await store.save(state_serialized)
     return deserialize_state(await store.read())
 
 
-async def read_state(client_id) -> str:
+async def read_state(client_id) -> dict:
     store = Store(client_id)
     return deserialize_state(await store.read())
