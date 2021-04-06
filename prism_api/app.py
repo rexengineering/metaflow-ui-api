4d549a790cd651b359383634300922b704a2f56f
Meta Engineer <m@plankc.com>
2021-04-06 17:53:51 -0600

    Delete files not part of this feature

--- prism_api/app.py
+++ prism_api/app.py
@@ -4,7 +4,6 @@ import sys
 from fastapi import FastAPI
 
 from prism_api.graphql.app import app as graphql_app
-from prism_api.rexflow.endpoints import router as rexflow_router
 from prism_api.state_manager.router import router as state_router
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
@@ -17,5 +16,4 @@ async def root():
     return 'Hello World'
 
 app.mount('/query', graphql_app)
-app.include_router(rexflow_router, prefix='/rexflow')
 app.include_router(state_router)
