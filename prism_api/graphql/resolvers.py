79e5507aa930faa1352cf65714e1c45404a94a4b
Meta Engineer <m@plankc.com>
2021-04-08 11:37:50 -0600

    Add mock resolvers for queries

--- prism_api/graphql/resolvers.py
+++ prism_api/graphql/resolvers.py
@@ -1,11 +1,41 @@
-from typing import Optional
+from ariadne import QueryType, ObjectType
 
-from ariadne import QueryType
+from prism_api.rexflow import entities as rxen
 
 
 query = QueryType()
 
 
-@query.field('hello')
-async def resolve_hello(*_, name: Optional[str] = 'World'):
-    return 'Hello ' + name
+@query.field('session')
+async def resolve_session(*_):
+    # TODO add model for Session
+    return {
+        'id': '',
+        'state': '',
+    }
+
+
+@query.field('workflows')
+async def resolve_workflows(*_):
+    return {}
+
+
+workflow_query = ObjectType('WorkflowQuery')
+
+
+@workflow_query.field('active')
+async def resolve_workflow_active(obj, *_, filter: rxen.WorkflowFilter = None):
+    return [
+        rxen.Workflow(
+            iid='test',
+            status=rxen.WorkflowStatus.IN_PROGRESS,
+        )
+    ]
+
+
+@workflow_query.field('available')
+async def resolve_workflow_available(obj, *_):
+    return [
+        '123',
+        '456',
+    ]
