3899556bb34306640cf6ca2a88875a49681e91dc
Meta Engineer <m@plankc.com>
2021-04-08 17:09:07 -0600

    Add api to communicate with rexflow bridge

---
+++ prism_api/rexflow/api.py
@@ -0,0 +1,107 @@
+"""Interface to interact with REXFlow"""
+import asyncio
+from collections import defaultdict
+import itertools
+from typing import Dict, List
+
+from pydantic import validate_arguments
+
+from . import entities as e
+from .bridge import REXFlowBridgeGQL as REXFlowBridge
+
+
+workflows: Dict[e.WorkflowInstanceId, e.Workflow] = {}
+
+
+async def get_available_workflows() -> List[e.WorkflowDeploymentId]:
+    return ['123']
+
+
+async def start_workflow(
+    deployment_id: e.WorkflowDeploymentId,
+) -> e.Workflow:
+    workflow = await REXFlowBridge.start_workflow(
+        deployment_id=deployment_id,
+    )
+    workflows[workflow.iid] = workflow
+    return workflow
+
+
+async def get_active_workflows() -> List[e.Workflow]:
+    return workflows.values()
+
+
+async def complete_workflow(
+    instance_id: e.WorkflowInstanceId,
+) -> None:
+    del workflows[instance_id]
+
+
+@validate_arguments
+async def start_tasks(tasks: List[e.TaskStartInput]) -> None:
+    created_tasks = []
+    for task in tasks:
+        new_task = e.Task(
+            id=task.id,
+            data=[e.TaskData(**d.dict()) for d in task.data],
+        )
+        workflows[task.iid].tasks.append(new_task)
+        created_tasks.append(new_task)
+    return created_tasks
+
+
+async def _refresh_workflow(workflow: e.Workflow):
+    """Refresh a single workflow task"""
+    bridge = REXFlowBridge(workflow)
+    workflow.tasks = await bridge.get_task_data()
+
+
+async def refresh_workflows() -> None:
+    """Asyncrhonously refresh all workflows tasks"""
+    await asyncio.gather(*[
+        _refresh_workflow(workflow)
+        for workflow in workflows.values()
+    ])
+
+
+async def _save_tasks(
+    iid: e.WorkflowInstanceId,
+    tasks: List[e.Task],
+) -> List[e.Task]:
+    bridge = REXFlowBridge(workflows[iid])
+    return await bridge.save_task_data(tasks)
+
+
+@validate_arguments
+async def save_tasks(tasks: List[e.Task]) -> List[e.Task]:
+    workflow_instances = defaultdict(list)
+    for task in tasks:
+        workflow_instances[task.iid].append(task)
+    results = await asyncio.gather(*[
+        _save_tasks(iid, tasks)
+        for iid, tasks in workflow_instances
+    ])
+
+    return list(itertools.chain(*results))
+
+
+async def _complete_tasks(
+    iid: e.WorkflowInstanceId,
+    tasks: List[e.Task],
+) -> List[e.Task]:
+    bridge = REXFlowBridge(workflows[iid])
+    return await bridge.complete_task(tasks)
+
+
+async def complete_tasks(
+    tasks: List[e.Task],
+) -> List[e.Task]:
+    workflow_instances = defaultdict(list)
+    for task in tasks:
+        workflow_instances[task.iid].append(task)
+    results = await asyncio.gather(*[
+        _complete_tasks(iid, tasks)
+        for iid, tasks in workflow_instances
+    ])
+
+    return list(itertools.chain(*results))
