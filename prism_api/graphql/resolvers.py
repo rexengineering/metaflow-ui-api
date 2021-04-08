2dcfa3a8a259001438996db6e27a3b0e3a08dd5e
Meta Engineer <m@plankc.com>
2021-04-08 12:42:42 -0600

    Change queries and mutations to an object approach

--- prism_api/graphql/resolvers.py
+++ prism_api/graphql/resolvers.py
@@ -1,7 +1,10 @@
-from ariadne import QueryType, ObjectType, MutationType
+import logging
+
+from ariadne import QueryType, MutationType
 
 from prism_api.rexflow import entities as rxen
 
+logger = logging.getLogger(__name__)
 
 query = QueryType()
 
@@ -15,72 +18,63 @@ async def resolve_session(*_):
     }
 
 
-@query.field('workflows')
-async def resolve_workflows(*_):
-    return {}
+class WorkflowResolver:
+    def __init__(self, *_):
+        pass
+
+    async def active(
+        self,
+        *_,
+        filter: rxen.WorkflowFilter = None
+    ):
+        return [
+            rxen.Workflow(
+                iid='test',
+                status=rxen.WorkflowStatus.IN_PROGRESS,
+            )
+        ]
+
+    async def available(self, *_):
+        return [
+            '123',
+            '456',
+        ]
 
 
-workflow_query = ObjectType('WorkflowQuery')
-
-
-@workflow_query.field('active')
-async def resolve_workflow_active(obj, *_, filter: rxen.WorkflowFilter = None):
-    return [
-        rxen.Workflow(
-            iid='test',
-            status=rxen.WorkflowStatus.IN_PROGRESS,
-        )
-    ]
-
-
-@workflow_query.field('available')
-async def resolve_workflow_available(obj, *_):
-    return [
-        '123',
-        '456',
-    ]
+query.set_field('workflows', WorkflowResolver)
 
 
 mutation = MutationType()
 
 
-@mutation.field('session')
-async def resolve_session_mutation(*_):
-    return {}
+class StateMutations:
+    async def update(*_, input):
+        return {
+            'status': rxen.OperationStatus.SUCCESS,
+            'state': ''
+        }
 
 
-session_mutations = ObjectType('SessionMutations')
+class SessionMutations:
+    def __init__(sel, *_) -> None:
+        pass
+
+    async def start(self, _):
+        return {
+            'status': rxen.OperationStatus.SUCCESS,
+            'session': {
+                'id': '',
+                'state': '',
+            },
+        }
+
+    async def state(self, _):
+        return StateMutations()
+
+    async def close(self, _):
+        return {
+            'status': rxen.OperationStatus.SUCCESS,
+        }
 
 
-@session_mutations.field('start')
-async def resolve_session_start(*_):
-    return {
-        'status': rxen.OperationStatus.SUCCESS,
-        'session': {
-            'id': '',
-            'state': '',
-        },
-    }
-
-
-@session_mutations.field('state')
-async def resolve_session_state(*_):
-    return {}
-
-
-state_mutations = ObjectType('StateMutations')
-
-
-@state_mutations.field('update')
-async def resolve_session_state_update(*_, input):
-    return {
-        'status': rxen.OperationStatus.SUCCESS,
-        'state': ''
-    }
-
-
-@session_mutations.field('close')
-async def resolve_session_close(*_):
-    return {
-        'status': rxen.OperationStatus.SUCCESS,
-    }
+mutation.set_field('session', SessionMutations)
--- prism_api/graphql/schema.py
+++ prism_api/graphql/schema.py
@@ -8,8 +8,5 @@ typedefs = ariadne.load_schema_from_path('./schema/')
 schema = ariadne.make_executable_schema(
     typedefs,
     res.query,
-    res.workflow_query,
     res.mutation,
-    res.session_mutations,
-    res.state_mutations,
 )
