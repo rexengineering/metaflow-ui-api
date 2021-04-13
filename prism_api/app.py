ab5e9d18ac5fb9094580b708a028266dba1ce9ac
Meta Engineer <m@plankc.com>
2021-04-13 12:54:52 -0600

    Add cors configuration to fastapi

--- prism_api/app.py
+++ prism_api/app.py
@@ -2,13 +2,22 @@ import logging
 import sys
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+from prism_api import settings
 from prism_api.graphql.app import app as graphql_app
 from prism_api.state_manager.router import router as state_router
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 app = FastAPI()
+
+origins = settings.CORS_ORIGINS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=['*'],
+)
 
 
 @app.get('/')
--- prism_api/settings.py
+++ prism_api/settings.py
@@ -2,6 +2,11 @@
 import os
 
 
-DEBUG = True
+DEBUG = os.getenv('APP_DEBUG', 'true').lower() == 'true'
+CORS_ORIGINS = [
+    origin.strip()
+    for origin in os.getenv('APP_CORS_ORIGINS', '').split(',')
+]
+
 REXFLOW_HOST = os.getenv('REX_REXFLOW_HOST')
 REXFLOW_HOST_INSTANCE = os.getenv('REX_REXFLOW_HOST_INSTANCE')
