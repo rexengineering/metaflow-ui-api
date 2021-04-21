b1e4fdbaa931bc74b820eb38c911e1ed431df19b
Meta Engineer <m@plankc.com>
2021-04-21 11:05:58 -0600

    Move schema inside package

--- prism_api/graphql/schema.py
+++ prism_api/graphql/schema.py
@@ -1,9 +1,14 @@
+from os import path
+
 import ariadne
 
 from . import resolvers as res
 
+basepath = path.dirname(__file__)
+schemapath = path.abspath(path.join(basepath, 'schema'))
 
-typedefs = ariadne.load_schema_from_path('./schema/')
+
+typedefs = ariadne.load_schema_from_path(schemapath)
 
 schema = ariadne.make_executable_schema(
     typedefs,
