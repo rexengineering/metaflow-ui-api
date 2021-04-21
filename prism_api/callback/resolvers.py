0eadcd6645030fa10e0fad882c92db2fca226ba9
Meta Engineer <m@plankc.com>
2021-04-21 12:30:07 -0600

    Add pydantic validation to resolvers

--- prism_api/callback/resolvers.py
+++ prism_api/callback/resolvers.py
@@ -1,4 +1,8 @@
 from ariadne import QueryType, MutationType
+from pydantic import validate_arguments
+
+from . import entities as w
+from prism_api.rexflow.entities import types as e
 
 
 query = QueryType()
@@ -16,10 +20,11 @@ class Task:
     def __init__(self, *_) -> None:
         pass
 
-    async def start(self, info, input):
-        return {
-            'status': 'SUCCESS',
-        }
+    @validate_arguments
+    async def start(self, info, input: w.StartTaskInput):
+        return w.StartTaskPayload(
+            status=e.OperationStatus.SUCCESS,
+        )
 
 
 mutation.set_field('task', Task)
@@ -29,10 +34,11 @@ class Workflow:
     def __init__(self, *_) -> None:
         pass
 
-    async def complete(self, info, input):
-        return {
-            'status': 'SUCCESS',
-        }
+    @validate_arguments
+    async def complete(self, info, input: w.CompleteWorkflowInput):
+        return w.CompleteWorkflowPayload(
+            status=e.OperationStatus.SUCCESS,
+        )
 
 
 mutation.set_field('workflow', Workflow)
