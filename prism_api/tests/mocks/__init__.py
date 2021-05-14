7ccb48504c354d577fee92f35f2d38538259a7ad
Meta Engineer <m@plankc.com>
2021-05-14 11:38:27 -0600

    Add some missing types

--- prism_api/tests/mocks/__init__.py
+++ prism_api/tests/mocks/__init__.py
@@ -1,4 +1,10 @@
+from prism_api.rexflow.entities.types import (
+    WorkflowDeploymentId,
+    WorkflowInstanceId,
+    TaskId,
+)
 
-MOCK_DID = 'process-123-abc'
-MOCK_IID = 'process-123-abc-12345678'
-MOCK_TID = 'Activity_abcde'
+
+MOCK_DID: WorkflowDeploymentId = 'process-123-abc'
+MOCK_IID: WorkflowInstanceId = 'process-123-abc-12345678'
+MOCK_TID: TaskId = 'Activity_abcde'
