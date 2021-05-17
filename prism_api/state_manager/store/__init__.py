54ebe1ff107341bba847a38752da51a45257be0b
Meta Engineer <m@plankc.com>
2021-05-17 17:23:46 -0600

    Fix PR comments

--- prism_api/state_manager/store/__init__.py
+++ prism_api/state_manager/store/__init__.py
@@ -1,7 +1,6 @@
-from . import api
-
-
-read_state = api.read_state
-save_state = api.save_state
-read_raw_state = api.read_raw_state
-save_raw_state = api.save_raw_state
+from .api import (  # noqa: F401
+    read_state,
+    save_state,
+    read_raw_state,
+    save_raw_state,
+)
