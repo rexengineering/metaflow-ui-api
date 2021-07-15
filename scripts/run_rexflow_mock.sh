d2a771e4e8300f03b136fc9ae6a911a3daa6a7ba
Meta Engineer <m@plankc.com>
2021-07-15 11:19:34 -0600

    Add mock configuration to docker compose

--- scripts/run_rexflow_mock.sh
+++ scripts/run_rexflow_mock.sh
@@ -1,3 +1,6 @@
-echo "Start rexflow mock server"
+#!/usr/bin/env bash
+source activate prism-api
+export PYTHONPATH=${PYTHONPATH}:.
 
+echo "Start rexflow mock server"
 uvicorn rexflow_mock.app:app --host 0.0.0.0 --port 8001 --reload
