5f3748982eb9f22ba2660ea370d17400e8225854
Meta Engineer <m@plankc.com>
2021-08-25 12:36:36 -0600

    Add debug commands to prism-api

---
+++ prism_api/cli/commands.py
@@ -0,0 +1,48 @@
+import asyncio
+import click
+
+from prism_api import settings
+from prism_api.rexflow import api as rexflow
+
+
+async def _a_cancel_workflows():
+    click.echo('Refreshing workflows')
+    await rexflow.refresh_workflows()
+    click.echo('Canceling running workflows')
+    running_workflows = await rexflow.get_active_workflows(
+        'anon',
+        [],
+    )
+    count = 0
+    for workflow in running_workflows:
+        click.echo(f'Canceling workflow {workflow.iid}')
+        await rexflow.cancel_workflow(workflow.iid)
+        count = count + 1
+
+    click.echo(f'{count} workflows canceled')
+
+
+@click.command()
+def cancel_workflows():
+    if settings.DEBUG:
+        asyncio.run(_a_cancel_workflows())
+    else:
+        click.echo('This command can only be executed on debug mode')
+
+
+async def _a_refresh_workflows():
+    click.echo('Refreshing workflows')
+    await rexflow.refresh_workflows()
+    running_workflows = await rexflow.get_active_workflows(
+        'anon',
+        [],
+    )
+    click.echo(str(running_workflows))
+
+
+@click.command()
+def refresh_workflows():
+    if settings.DEBUG:
+        asyncio.run(_a_refresh_workflows())
+    else:
+        click.echo('This command can only be executed on debug mode')
