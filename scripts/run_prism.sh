a7a198c7286af47c5c2723458e63d371b821fecf
Meta Engineer <m@plankc.com>
2021-08-27 09:56:39 -0600

    Add a 60 seconds timeout for workers

--- scripts/run_prism.sh
+++ scripts/run_prism.sh
@@ -4,4 +4,4 @@ source activate prism-api
 export PYTHONPATH=${PYTHONPATH}:.
 
 echo 'starting gunicorn with uvicorn workers now'
-gunicorn prism_api.app:app -w 4 -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:8000
+gunicorn prism_api.app:app -w 4 -k uvicorn.workers.UvicornWorker --timeout 60 --bind 0.0.0.0:8000
