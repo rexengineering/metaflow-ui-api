15b0289ae287426b7c47188901386c47dc7ad68b
Meta Engineer <m@plankc.com>
2021-09-13 12:27:20 -0600

    Move callback resolver tests to new package

---
+++ prism_api/tests/callback/test_resolvers.py
@@ -0,0 +1,118 @@
+import unittest
+from unittest import mock
+
+import pytest
+
+from ..mocks import (
+    MOCK_IID,
+    MOCK_TID,
+    rexflow_api,
+)
+from ..mocks.graphql_info import MockInfo
+from ..utils import run_async
+from prism_api.callback.resolvers import (
+    TaskMutations as TaskCallbackMutations,
+    WorkflowMutations as WorkflowCallbackMutations,
+)
+from prism_api.callback.entities import (
+    CompleteWorkflowInput,
+    CompleteWorkflowPayload,
+    StartTaskInput,
+    StartTaskPayload,
+)
+from prism_api.rexflow.entities.types import (
+    OperationStatus,
+)
+from prism_api.rexflow.errors import BridgeNotReachableError
+
+
+def _raise_bridge_exception(*_, **__):
+    raise BridgeNotReachableError('test')
+
+
+def _raise_exception(*_, **__):
+    raise Exception('test')
+
+
+@pytest.mark.ci
+@mock.patch(
+    'prism_api.callback.resolvers.api',
+    rexflow_api,
+)
+class TestCallBackResolvers(unittest.TestCase):
+    @run_async
+    async def test_start_task_callback(self):
+        mutations = TaskCallbackMutations()
+        response = await mutations.start(
+            MockInfo(),
+            input=StartTaskInput(
+                iid=MOCK_IID,
+                tid=MOCK_TID,
+            ),
+        )
+        self.assertIsInstance(response, StartTaskPayload)
+        self.assertEqual(response.status, OperationStatus.SUCCESS)
+
+    @run_async
+    async def test_complete_workflow_callback(self):
+        mutations = WorkflowCallbackMutations()
+        response = await mutations.complete(
+            MockInfo(),
+            input=CompleteWorkflowInput(
+                iid=MOCK_IID,
+            ),
+        )
+        self.assertIsInstance(response, CompleteWorkflowPayload)
+        self.assertEqual(response.status, OperationStatus.SUCCESS)
+
+
+@pytest.mark.ci
+class TestCallBackResolversErrors(unittest.TestCase):
+    @run_async
+    async def test_start_task_mutation_errors(self):
+        mutations = TaskCallbackMutations()
+        with mock.patch(
+            'prism_api.callback.resolvers.api.start_tasks',
+        ) as call:
+            call.side_effect = _raise_bridge_exception
+            response = await mutations.start(
+                MockInfo(),
+                input=StartTaskInput(
+                    iid=MOCK_IID,
+                    tid=MOCK_TID,
+                ),
+            )
+
+        self.assertIsInstance(response, StartTaskPayload)
+        self.assertEqual(response.status, OperationStatus.FAILURE)
+
+        with mock.patch(
+            'prism_api.callback.resolvers.api.start_tasks',
+        ) as call:
+            call.side_effect = _raise_exception
+            response = await mutations.start(
+                MockInfo(),
+                input=StartTaskInput(
+                    iid=MOCK_IID,
+                    tid=MOCK_TID,
+                ),
+            )
+
+        self.assertIsInstance(response, StartTaskPayload)
+        self.assertEqual(response.status, OperationStatus.FAILURE)
+
+    @run_async
+    async def test_complete_workflow_callback_errors(self):
+        mutations = WorkflowCallbackMutations()
+        with mock.patch(
+            'prism_api.callback.resolvers.api.complete_workflow',
+        ) as call:
+            call.side_effect = _raise_exception
+            response = await mutations.complete(
+                MockInfo(),
+                input=CompleteWorkflowInput(
+                    iid=MOCK_IID,
+                ),
+            )
+        self.assertIsInstance(response, CompleteWorkflowPayload)
+        self.assertEqual(response.status, OperationStatus.FAILURE)
--- prism_api/tests/graphql/test_resolvers.py
+++ prism_api/tests/graphql/test_resolvers.py
@@ -15,16 +15,6 @@ from ..mocks import (
 from ..mocks.graphql_info import MockInfo
 from ..mocks.state_store import FakeStore
 from ..utils import run_async
-from prism_api.callback.resolvers import (
-    TaskMutations as TaskCallbackMutations,
-    WorkflowMutations as WorkflowCallbackMutations,
-)
-from prism_api.callback.entities import (
-    CompleteWorkflowInput,
-    CompleteWorkflowPayload,
-    StartTaskInput,
-    StartTaskPayload,
-)
 from prism_api.graphql.resolvers import (
     resolve_session,
     WorkflowResolver,
@@ -49,15 +39,6 @@ from prism_api.rexflow.entities.types import (
     Workflow,
     WorkflowDeployment,
 )
-from prism_api.rexflow.errors import BridgeNotReachableError
-
-
-def _raise_bridge_exception(*_, **__):
-    raise BridgeNotReachableError('test')
-
-
-def _raise_exception(*_, **__):
-    raise Exception('test')
 
 
 async def mock_get_deployments():
@@ -240,87 +221,3 @@ class TestRexflowResolversErrors(unittest.TestCase):
         self.assertGreater(len(response.errors), 0)
         for error in response.errors:
             self.assertIsInstance(error, Problem)
-
-
-@pytest.mark.ci
-@mock.patch(
-    'prism_api.callback.resolvers.api',
-    rexflow_api,
-)
-class TestCallBackResolvers(unittest.TestCase):
-    @run_async
-    async def test_start_task_callback(self):
-        mutations = TaskCallbackMutations()
-        response = await mutations.start(
-            MockInfo(),
-            input=StartTaskInput(
-                iid=MOCK_IID,
-                tid=MOCK_TID,
-            ),
-        )
-        self.assertIsInstance(response, StartTaskPayload)
-        self.assertEqual(response.status, OperationStatus.SUCCESS)
-
-    @run_async
-    async def test_complete_workflow_callback(self):
-        mutations = WorkflowCallbackMutations()
-        response = await mutations.complete(
-            MockInfo(),
-            input=CompleteWorkflowInput(
-                iid=MOCK_IID,
-            ),
-        )
-        self.assertIsInstance(response, CompleteWorkflowPayload)
-        self.assertEqual(response.status, OperationStatus.SUCCESS)
-
-
-@pytest.mark.ci
-class TestCallBackResolversErrors(unittest.TestCase):
-    @run_async
-    async def test_start_task_mutation_errors(self):
-        mutations = TaskCallbackMutations()
-        with mock.patch(
-            'prism_api.callback.resolvers.api.start_tasks',
-        ) as call:
-            call.side_effect = _raise_bridge_exception
-            response = await mutations.start(
-                MockInfo(),
-                input=StartTaskInput(
-                    iid=MOCK_IID,
-                    tid=MOCK_TID,
-                ),
-            )
-
-        self.assertIsInstance(response, StartTaskPayload)
-        self.assertEqual(response.status, OperationStatus.FAILURE)
-
-        with mock.patch(
-            'prism_api.callback.resolvers.api.start_tasks',
-        ) as call:
-            call.side_effect = _raise_exception
-            response = await mutations.start(
-                MockInfo(),
-                input=StartTaskInput(
-                    iid=MOCK_IID,
-                    tid=MOCK_TID,
-                ),
-            )
-
-        self.assertIsInstance(response, StartTaskPayload)
-        self.assertEqual(response.status, OperationStatus.FAILURE)
-
-    @run_async
-    async def test_complete_workflow_callback_errors(self):
-        mutations = WorkflowCallbackMutations()
-        with mock.patch(
-            'prism_api.callback.resolvers.api.complete_workflow',
-        ) as call:
-            call.side_effect = _raise_exception
-            response = await mutations.complete(
-                MockInfo(),
-                input=CompleteWorkflowInput(
-                    iid=MOCK_IID,
-                ),
-            )
-        self.assertIsInstance(response, CompleteWorkflowPayload)
-        self.assertEqual(response.status, OperationStatus.FAILURE)
